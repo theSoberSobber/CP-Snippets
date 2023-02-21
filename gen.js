af90dbfe1fe08609949655df9a57431dec021123
theSoberSobber <akshatshrivastava0010@gmail.com>
2023-02-22 01:53:04 +0530

    auto index

--- gen.js
+++ gen.js
@@ -1,1 +1,26 @@
-console.log("ok");
+const {readFile, writeFile} = require("fs/promises");
+const base = "https://github.com/theSoberSobber/CP-Snippets/blob/main/snippets.json";
+
+(async () => {
+    let file = await JSON.parse(await readFile('./snippets.json', 'utf8'));
+    let links = "";
+    let pre = {};
+    let t=2;
+    for(let i in file){
+        pre[i]=t;
+        t+=file[i].body.length+6;
+    }
+    for(let i in file){
+        const lno=1;
+        links += 
+`- [${i}](${base}#L${pre[i]})
+        
+`;
+    }
+    let topics = `
+# Index - 
+
+${links}`;
+    // console.log(topics);
+    await writeFile('./README.md', topics);
+})();
