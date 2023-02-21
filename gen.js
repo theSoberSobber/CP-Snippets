8dfaa5ff39100ca6d7b63d1497d230034c1d5341
theSoberSobber <akshatshrivastava0010@gmail.com>
2023-02-22 02:05:45 +0530

    style

--- gen.js
+++ gen.js
@@ -1,26 +1,23 @@
-const {readFile, writeFile} = require("fs/promises");
-const base = "https://github.com/theSoberSobber/CP-Snippets/blob/main/snippets.json";
+const {readFile, writeFile} = require("fs/promises"); 
+const base = "https://github.com/theSoberSobber/CP-Snippets/blob/main/snippets.json"; 
+(async () => { 
+    let file = await JSON.parse(await readFile('./snippets.json', 'utf8')); 
+    let links = ""; 
+    let pre = {}; 
+    let t=2; 
+    for(let i in file){ 
+        pre[i]=t; 
+        t+=file[i].body.length+6; 
+    } 
+    for(let i in file){ 
+        const lno=1; 
+        links += `
+- **[${i}](${base}#L${pre[i]})** : ${file[i].description} `;
+    } 
+    let topics = `# Index - 
 
-(async () => {
-    let file = await JSON.parse(await readFile('./snippets.json', 'utf8'));
-    let links = "";
-    let pre = {};
-    let t=2;
-    for(let i in file){
-        pre[i]=t;
-        t+=file[i].body.length+6;
-    }
-    for(let i in file){
-        const lno=1;
-        links += 
-`- [${i}](${base}#L${pre[i]})
-        
-`;
-    }
-    let topics = `
-# Index - 
-
-${links}`;
-    // console.log(topics);
-    await writeFile('./README.md', topics);
+--- 
+${links}`; 
+    console.log(topics);
+    // await writeFile('./README.md', topics); 
 })();
