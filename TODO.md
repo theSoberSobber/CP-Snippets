e63419364b7acf1c661bb01f27367e4c58e05cc5
theSoberSobber <akshatshrivastava0010@gmail.com>
2023-03-10 15:45:24 +0530

    stylized

--- TODO.md
+++ TODO.md
@@ -8,6 +8,17 @@
 
 # Repo - 
 
-- make it actually look good
+- make it actually look good (Done, kinda)
 - map the gh index to anchor tags by ids of the html page
 - make a frontend for the snippet-gen script
+- multi-page-site (index that redirects to codes with a fixed navbar)
+- generalise the markdown site-gen engine and seperate it from just the codes page
+    - split site-gen into
+        - site-gen: converts md to html
+        - code-gen: json snippets to code
+- new workflow
+    - make new file in snippets folder
+    - snippet-gen converts it to snippet and adds it to the snippets.json file
+- generate initial snippets folder from snippets.json using site-gen's JSON to code generation module
+- make the index variables global and import them
+- move site and gh page deployment to a site folder
--- scripts/site-gen.js
+++ scripts/site-gen.js
@@ -1,6 +1,25 @@
+const css =`<style>
+:root{--background-color:#EEEEEE;--text:#444444;--link-color:teal;}
+@media(prefers-color-scheme:dark){
+:root{--background-color:#222222;--text:#CCCCCC;--link-color:cadetblue;}}
+body{background:var(--background-color);color:var(--text);}
+body{margin:30px auto;max-width:700px;line-height:1.6;font-size:18px;font-family:Arial,Helvetica,sans-serif;color:var(--text);background:var(--background-color);padding:0 10px;}
+h1{line-height:1.2;text-align:center;}
+h2,h3{line-height:1.2;}
+td,th{border:none!important;}
+table{width:100%!important;}
+a{color:var(--link-color);text-decoration:none;}
+pre code{-webkit-overflow-scrolling: touch; display: block; overflow-x: scroll;}
+</style>`;
+
+const base = "https://github.com/theSoberSobber/CP-Snippets/blob/main/snippets.json"; 
+const self = "https://thesobersobber.github.io/CP-Snippets";
+const gh = "https://github.com/theSoberSobber"; 
+const ld = "https://www.linkedin.com/in/pavitchhabrawork"
+const cf = "https://codeforces.com"
+
 const {readFile, writeFile} = require("fs/promises"); 
 const { marked } = require('marked');
-const base = "https://github.com/theSoberSobber/CP-Snippets/blob/main/snippets.json"; 
 
 (async () => { 
     let file = await JSON.parse(await readFile('./snippets.json', 'utf8')); 
@@ -12,7 +31,15 @@ const base = "https://github.com/theSoberSobber/CP-Snippets/blob/main/snippets.j
         t+=file[i].body.length+6; 
     }
     let final=`
-# CP Snippets - 
+# CP Snippets
+
+| [About](${self}) | [Codeforces](${cf}) | [GitHub](${gh}) | [LinkedIn](${ld}) |
+| - | - | - | - |
+
+## About
+
+A collection of CPP Snippets to aid in competetive programming. <br />
+This site was auto generated with the help of [marked](https://www.npmjs.com/package/marked).
 
 ---
 
@@ -37,5 +64,5 @@ ${code}
 ---
 `;
     }
-    await writeFile('./index.html', marked.parse(final)); 
+    await writeFile('./index.html', css+marked.parse(final)); 
 })();
