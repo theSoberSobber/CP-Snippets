6ec23384232f01a606b8b9df5e7d24d1fc0093c4
Pavit <109434814+theSoberSobber@users.noreply.github.com>
2023-03-20 16:24:33 +0530

    interactive problems

---
+++ codes/interactive.cpp
@@ -0,0 +1,21 @@
+//interactive
+//essential measures for interactive problems
+
+void solve(){
+    int n; cin>>n;
+    
+    auto querySystem = [&](int l, int r) {
+        // print your query
+        cout<<r-l+1<<endl;
+        cout<<endl;
+        // receive and return reply from system
+        int wt; cin>>wt;
+        return wt;
+    };
+
+    // write your logic here and use querySystem to receive answer to query
+    // do a cout<<endl after each cout
+
+    
+    cout<<endl;
+}
