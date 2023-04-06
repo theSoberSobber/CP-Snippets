2e9c91241853ddb775be726a68c9d42f768cb590
Pavit <109434814+theSoberSobber@users.noreply.github.com>
2023-04-06 13:56:05 +0530

    cute tricks

---
+++ codes/cute-lcm.cpp
@@ -0,0 +1,6 @@
+//cute-lcm
+//[a,b,c]=abc(a,b,c)/(a,b)(b,c)(c,a), where []=lcm adn ()=gcd or [a,b,c]=abc/gcd(ab,bc,ca)
+
+"https://math.stackexchange.com/questions/1579/n-ary-version-of-gcda-b-space-lcma-b-ab"
+"N-ary versions of gcd and lcm"
+"proof is heavy lattice ordered smthing based or use inclusion exclusion"
