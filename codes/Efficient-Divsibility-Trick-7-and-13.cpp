aeb07d0ceb3fb5a55d239b69c94b36c8c5e101c3
theSoberSobber <109434814+theSoberSobber@users.noreply.github.com>
2024-12-26 21:14:52 +0530

    Create Efficient-Divsibility-Trick-7-and-13.cpp

---
+++ codes/Efficient-Divsibility-Trick-7-and-13.cpp
@@ -0,0 +1,4 @@
+// Efficient Divibility Trick for 7 and 13
+// used here: https://codeforces.com/contest/2043/problem/B
+
+https://www.mathematicalgemstones.com/gemstones/pearl/a-faster-divisibility-rule-for-7/
